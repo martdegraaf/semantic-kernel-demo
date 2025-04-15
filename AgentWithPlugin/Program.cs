d447b0534b90f225c138ee882207945c1805ef1f
Mart de Graaf <mart@martdegraaf.nl>
2025-04-15 20:41:01 +0200

    Add OpenApi Sample with Plugin

---
+++ AgentWithPlugin/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.ChatCompletion;
+using DotNetEnv;
+
+// Build configuration
+// Load .env file
+Env.Load();
+
+// Now you can access them as environment variables
+var deploymentName = Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT")
+                     ?? throw new InvalidOperationException("AZURE_OPENAI_DEPLOYMENT is not set.");
+var endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT")
+               ?? throw new InvalidOperationException("AZURE_OPENAI_ENDPOINT is not set.");
+var apiKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY")
+             ?? throw new InvalidOperationException("AZURE_OPENAI_API_KEY is not set.");
+
+
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion(deploymentName, endpoint, apiKey);
+
+
+var kernel = builder.Build();
+
+kernel.Plugins.Add(KernelPluginFactory.CreateFromType<MenuPlugin>());
+
+ChatCompletionAgent agent =
+    new()
+    {
+        Name = "SK-Assistant",
+        Instructions = "You are a helpful assistant.",
+        Kernel = kernel,
+        Arguments = new KernelArguments(new PromptExecutionSettings() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() })
+
+    };
+
+await foreach (AgentResponseItem<ChatMessageContent> response 
+    in agent.InvokeAsync("What is the price of the soup special?"))
+{
+    Console.WriteLine(response.Message);
+}
+
+sealed class MenuPlugin
+{
+    [KernelFunction, Description("Provides a list of specials from the menu.")]
+    public string GetSpecials() =>
+        """
+        Special Soup: Clam Chowder
+        Special Salad: Cobb Salad
+        Special Drink: Chai Tea
+        """;
+
+    [KernelFunction, Description("Provides the price of the requested menu item.")]
+    public string GetItemPrice(
+        [Description("The name of the menu item.")]
+        string menuItem) =>
+        "$9.99";
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # semantic-kernel-demo
+
+Demos for my Semantic Kernel talk.
+
+
+## Prerequisites
+
+To run the codetours use the [Codetour extension](https://marketplace.visualstudio.com/items?itemName=ms-vscode.codetour) for Visual Studio Code with the C# DevKit.
+
+## Links
+
+- https://learn.microsoft.com/en-us/semantic-kernel/overview/
+
+- https://ollama.com/download/windows
+- https://huggingface.co/
+- https://allardschuurmans.nl/starting-with-semantic-kernel-to-create-an-ai-agent/
